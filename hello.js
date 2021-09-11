1ec0372f74d26215fe9ebdf5365068a2fa3f7097
patankhalid <patankhalid143@gmail.com>
2021-09-11 16:52:55 +0530

    added data

---
+++ hello.js
@@ -0,0 +1,31 @@
+var d1="Hello Patan tttttt tttttt ttttttt"
+
+var name="patan"
+var NAME="mysa"
+var Name="barath"
+var name="large"
+
+console.log(name)
+console.log(NAME)
+console.log(Name)
+
+
+
+
+
+
+
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+
+// console.log(true)
+// console.log(false)
+// console.log(12)
+// console.log(12.098)
+// console.log("true")
