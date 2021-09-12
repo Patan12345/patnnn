48de82b9300588f48b09c9d865b32dda1c80d27b
patankhalid <patankhalid143@gmail.com>
2021-09-12 10:46:43 +0530

    added some codes

--- hello.js
+++ hello.js
@@ -44,3 +44,70 @@
 // console.log(a*b)
 
 
+//syntax , rule
+
+
+//   if(condition){
+//      statment-1
+//   }
+
+  if(true){
+       console.log("Hey, I am under condition")
+       console.log("Hey, I am under condition")
+       console.log("Hey, I am under condition")
+       console.log("Hey, I am under condition")
+       console.log("Hey, I am under condition")
+  }
+  //false: undefined,null,0,""
+
+// var a=11
+
+//   if(a%2 == 0){
+//       console.log(a+"is even")
+//   }
+//   if(a%2 != 0){
+//     console.log(a+"is odd")
+//   }
+
+//   var a=10
+
+//   if(a%2 == 0){
+//     console.log(a+"is even")
+// }
+// if(a%2 != 0){
+//   console.log(a+"is odd")
+// }
+
+
+
+
+
+// 100,90
+// var q=99
+
+// if(q>100){
+//     console.log(q+"is greater than 100")
+// }else if(q<100){
+//     if(q>90){
+//         console.log(q+"is greater than 90")
+//     }else if(q<90){
+//         console.log(q+"is smaller than 90")
+//     }else{
+//         console.log(q+"is equals to 90")
+//     }
+//     console.log(q+"is smaller than 100")
+// }else{
+    // console.log(q+"is equals to 100")
+// }
+
+
+var n=10
+if(n>10){
+    console.log(1)
+}else if(n==100){
+    console.log(2)
+}else {
+    console.log(3)
+}if(n==10){
+    console.log(4)
+}
