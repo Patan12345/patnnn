6dda35c3b4c38a886e67aec8e04004c6144996c5
patankhalid <patankhalid143@gmail.com>
2021-09-17 11:02:55 +0530

    added some loop condition

---
+++ loop.js
@@ -0,0 +1,50 @@
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+// console.log("Hiiii")
+// console.log("Hello 2021")
+
+
+
+
+
+/**
+3-types of loop
+
+1. whileloop
+
+syntax:
+    while(condition){
+        statment
+        increment/decrement
+        statment
+        statment
+        statment
+        statment
+    }
+ */
+
+var a=10
+while(a<13){
+    console.log("hello 2021")
+    console.log("hiii")
+    a++
+    console.log(a)
+}
